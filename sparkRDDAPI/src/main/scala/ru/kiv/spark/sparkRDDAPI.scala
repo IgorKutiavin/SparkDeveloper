50baad14eb656e68fe77e90404e97a2571d25fc1
IgorKutiavin <kiv5561@gmail.com>
2025-05-24 06:21:25 +0000

    lesson 10 scala RDD API

--- sparkRDDAPI/src/main/scala/ru/kiv/spark/sparkRDDAPI.scala
+++ sparkRDDAPI/src/main/scala/ru/kiv/spark/sparkRDDAPI.scala
@@ -18,35 +18,24 @@ object sparkRDDAPI {
     var i = 0
     val tripRDD = sc.textFile(args(0)).filter(x => x.split(",")(0) != "VendorID")
       .map(x =>
-//      if(x.split(",")(0) != "VendorID")
         (tripData.getKey(x),tripData.getData(x))
-//      else (0,x)
     )
     val taxiRDD = sc.textFile(args(1)).filter(_.split(",")(0).replace("\"","") != "LocationID")
       .map(x =>
-//      if(x.split(",")(0).replace("\"","") != "LocationID")
         (taxiData.getKey(x), taxiData.getData(x))
-//      else (0,x)
     )
 
     val joinRDD = tripRDD.join(taxiRDD)
 
-    //val mapper = new ObjectMapper with ScalaObjectMapper
-
     val groupRDD = joinRDD.groupBy(x => (x._2._2.Zone,(x._2._1.tpep_dropoff_datetime.hourOfDay().get())))
 
-    groupRDD.take(2).foreach(println)
+//    groupRDD.take(2).foreach(println)
+//    println(s"tripRDD.count: ${tripRDD.count().toString}\ntaxiRDD.count: ${taxiRDD.count().toString}\njoinRDD.count: ${joinRDD.count().toString}")
 
     val headRDD = sc.parallelize(List("Zone,hour,count"))
     val outRDD = headRDD.union(groupRDD.map(x => (x._1._1 + "," + x._1._2.toString + "," + x._2.count(p => p._1 == p._1).toString)))
-    outRDD.saveAsTextFile(args(2))
 
-    println(s"tripRDD.count: ${tripRDD.count().toString}\ntaxiRDD.count: ${taxiRDD.count().toString}\njoinRDD.count: ${joinRDD.count().toString}")
-    //println("taxiRDD.count:" + taxiRDD.count().toString)
-    //tripRDD.sortBy(p=> p.split(",")(1)).take(40).foreach(println)
-    //tripRDD.take(10).foreach(println)
-    //taxiRDD.take(10).foreach(println)
-    //joinRDD.take(10).foreach(println)
+    outRDD.coalesce(2).saveAsTextFile(args(2))
 
   }
 }
