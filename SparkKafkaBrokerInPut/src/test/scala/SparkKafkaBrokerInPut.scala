5bf70c2bb1f490afc482ef96fe937c559250f361
IgorKutiavin <kiv5561@gmail.com>
2025-07-07 18:24:38 +0000

    Spark Streaming part 2

--- SparkKafkaBrokerInPut/src/test/scala/SparkKafkaBrokerInPut.scala
+++ SparkKafkaBrokerInPut/src/test/scala/SparkKafkaBrokerInPut.scala
@@ -10,16 +10,17 @@ object SparkKafkaBrokerInPut {
   def main(args: Array[String]) = {
 
     val config                 = ConfigFactory.load()
-    val pathCSVFile            = config.getString("")
-    val inputBootstrapServers  = config.getString("input.bootstrap.servers")
-    val inputTopic             = config.getString("input.topic")
+    val pathCSVFile            = config.getString("path_csv_file")
+    //val inputBootstrapServers  = config.getString("input.bootstrap.servers")
+    //val inputTopic             = config.getString("input.topic")
     val outputBootstrapServers = config.getString("output.bootstrap.servers")
     val outputTopic            = config.getString("output.topic")
-    val path2model             = config.getString("path2model")
-    val checkpointLocation     = config.getString("checkpointLocation")
+    //val path2model             = config.getString("path2model")
+    //val checkpointLocation     = config.getString("checkpointLocation")
 
     val spark = SparkSession.builder
       .appName("SparkKafkaBrokerInPut")
+      .master("local[*]")
       .getOrCreate()
 
     import spark.implicits._
@@ -28,7 +29,18 @@ object SparkKafkaBrokerInPut {
       .option("header", "true") // Если первая строка - заголовок
       .option("inferSchema", "true") // Автоматическое определение типов данных
       .option("delimiter", ",") // Разделитель, по умолчанию - запята
-      .csv(args(1))
+      .csv(pathCSVFile)
 
+    val json = df.toJSON
+    //json.show(10)
+    val query = json.select().writeStream
+      .format("kafka")
+      .outputMode("append")
+      .format("kafka")
+      .option("kafka.bootstrap.servers", outputBootstrapServers)
+      .option("topic", outputTopic)
+      .start()
+
+    query.awaitTermination()
   }
 }
