f07583f4ac50e69458ce1843f1fe0c4537d671a8
IgorKutiavin <kiv5561@gmail.com>
2025-07-15 06:34:48 +0000

    SparkKafkaBrokerOutPut part 1

---
+++ SparkKafkaBrokerOutPut/build.sbt
@@ -0,0 +1,22 @@
+import scala.collection.Seq
+
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.12.18"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "SparkKafkaBrokerOutPut"
+  )
+
+lazy val sparkVersion = "3.5.5"
+lazy val kafkaVersion = "3.5.0"
+
+libraryDependencies ++= Seq(
+  "com.typesafe"      % "config"                    % "1.4.2",
+  "org.apache.spark" %% "spark-sql"                 % sparkVersion % "provided",
+  "org.apache.spark"  % "spark-mllib_2.12"          % sparkVersion % "provided",
+  "org.apache.spark"  % "spark-sql-kafka-0-10_2.12" % sparkVersion,
+  "org.apache.kafka"  % "kafka-clients"             % kafkaVersion,
+  "org.slf4j"         %"slf4j-simple"               % "1.6.1"
+)
--- SparkKafkaBrokerOutPut/src/main/scala/ru/kiv/spark/SparkKafkaBrokerOutPut.scala
+++ SparkKafkaBrokerOutPut/src/main/scala/ru/kiv/spark/SparkKafkaBrokerOutPut.scala
@@ -1,2 +1,30 @@
 package ru.kiv.spark
 
+import com.typesafe.config.ConfigFactory
+import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types.StructType
+
+object SparkKafkaBrokerOutPut {
+  def main (args: Array[String]) = {
+
+    val config                 = ConfigFactory.load()
+    val pathCSVFile            = config.getString("path_csv_file")
+    val outputBootstrapServers  = config.getString("output.bootstrap.servers")
+    val outputTopic             = config.getString("output.topic")
+    val checkPointParh         = config.getString("checkpoint_path")
+    val csvPath                = config.getString("csv_files_path")
+
+    val spark = SparkSession.builder
+      .appName("SparkKafkaBrokerOutPut")
+      .master("local[*]")
+      .getOrCreate()
+
+    val df = spark.readStream
+      .format("kafka")
+      .option("kafka.bootstrap.servers", outputBootstrapServers)
+      .option("subscribe", outputTopic)
+      .load()
+
+    df.show(10)
+  }
+}
